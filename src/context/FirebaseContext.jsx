dc51ef820267cd6aca8c1aead319dbd570aa98d1
vardhan ganugula <gvardhan2004@gmail.com>
2025-01-06 21:49:58 +0530

    add socials and remove test

--- src/context/FirebaseContext.jsx
+++ src/context/FirebaseContext.jsx
@@ -41,9 +41,35 @@ export const FirebaseProvider = ({ children }) => {
     });
   };
 
-  
+  const submitProject = async (projectName, codeLink, demoLink, coverPic) => {
+    const picRef = ref(
+      storage,
+      `/uploads/projects/${Date.now()}-${coverPic.name}`
+    );
+    const picture = await uploadBytes(picRef, coverPic);
+    if(picture)
+    return await addDoc(collection(firestore, "projects"), {
+      time: Date.now(),
+      projectName,
+      codeLink,
+      demoLink,
+      coverPic: await getDownloadURL(picRef),
+    });
+  };
 
-  
+  const getProjects = async() => {
+    const projectsRef = collection(firestore, "projects");
+    let q;
+    if(lastDocument){
+      q = query(projectsRef, orderBy('time'), startAfter(lastDocument),limit(5))
+    }else{
+      q = query(projectsRef, orderBy('time'), limit(5))
+    }
+    const allDocs = await getDocs(q)
+    let allProjectDocs = allDocs.docs.map(doc => ({...doc.data(), id:doc.id}))
+    lastDocument = allDocs.docs[allDocs.docs.length - 1] || lastDocument
+    return allProjectDocs;
+  };
   
   const signIn = (email, password) => {
     return signInWithEmailAndPassword(auth, email, password);
@@ -65,10 +91,22 @@ export const FirebaseProvider = ({ children }) => {
     }
   };
 
-  
+  const deleteMsg = async (id) => {
+    const docRef = doc(firestore, 'messages', id)
+    await deleteDoc(docRef)
+  }
 
-  
-  
+  const deleteProject = async (id) => {
+    const docRef = doc(firestore, 'projects', id)
+    await deleteDoc(docRef)
+  }
+  const getMessages = async()=>{
+    const msgRef = collection(firestore, 'messages')
+    const q = query(msgRef, orderBy('time'))
+    const msgs = await getDocs(q)
+    let records = msgs.docs.map(doc => ({...doc.data(), id:doc.id}) )
+    return records
+  }
   const getAllProjects = async()=>{
     const projRef = collection(firestore, 'projects')
     const q = query(projRef, orderBy('time'))
