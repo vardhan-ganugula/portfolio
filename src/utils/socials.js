2671dfb18227b0b19c264db6ce5060c037646809
vardhan ganugula <gvardhan2004@gmail.com>
2025-02-27 01:26:06 +0530

    add socials

--- src/utils/socials.js
+++ src/utils/socials.js
@@ -1,4 +1,5 @@
 import { FaTwitter, FaLinkedinIn, FaGithub } from "react-icons/fa";
+import { SiLeetcode,SiCodechef  } from "react-icons/si";
 
 
 export const socials = [
@@ -19,5 +20,16 @@ export const socials = [
         text: "Github",
         link: "https://github.com/vardhan-ganugula",
         color: "bg-gray-800"
+    },
+    {
+        icon: SiLeetcode,
+        text: "Leetcode",
+        link: "https://leetcode.com/u/22r01a05e2/",
+        color: "bg-yellow-400"
+    },{
+        icon: SiCodechef,
+        text: "Codechef",
+        link: "https://www.codechef.com/users/vardhan5e2",
+        color: "bg-yellow-900"
     }
 ];
